3ebfdb8df9faa933eba14b0050278e51fe470817
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-08-12 22:31:23 -0500

    feat: you can download and play audio in the history page

--- src/screens/History.jsx
+++ src/screens/History.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
+import { downloadAudio, playAudio } from '../utils/audio'
 export default function History() {
     const user = useSelector(state => state.user.user)
     const [history, setHistory] = useState([])
-    console.log(history)
 
     useEffect(() => {
         axios.get('http://localhost:3000/user/history', { withCredentials: true })
@@ -15,6 +15,35 @@ export default function History() {
                 console.error(error)
             });
     }, [])
+
+    const getAudio = (id) => {
+        return axios.get('http://localhost:3000/text-to-speech/' + id, { 
+            withCredentials: true,
+            responseType: 'blob',  // Expect a blob response (binary data)
+        })
+        .then(response => {
+            // Create a URL for the blob data
+            return response.data
+        })
+        .catch(error => {
+            console.error(error);
+            return null
+        })
+    }
+
+    const getAndDownloadAudio = (id) => {
+        getAudio(id)
+            .then((audioFile) => {
+                downloadAudio(audioFile)
+            })
+    }
+    const getAndPlayAudio = (id) => {
+        getAudio(id)
+            .then((audioFile) => {
+                playAudio(audioFile)
+            })
+    }
+
     
 
 
@@ -31,13 +60,13 @@ export default function History() {
                 <h2>History</h2>
                 <div>
                     {
-                        history.map((item) => {
+                        history && history.map(({_id, text, date_unix, history_item_id}) => {
                             return (
-                                <div key={item.id} className='history-item'>
-                                    <div>{item.text}</div>
-                                    <div>{item.date_unix}</div>
-                                    <button>Download Audio</button>
-                                    <button>Play Audio</button>
+                                <div key={_id} className='history-item'>
+                                    <div>{text}</div>
+                                    <div>{date_unix}</div>
+                                    <button onClick={() => getAndDownloadAudio(history_item_id)}>Download Audio</button>
+                                    <button onClick={() => getAndPlayAudio(history_item_id)}>Play Audio</button>
                                 </div>
                             )
                         })
