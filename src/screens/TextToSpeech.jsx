8dab7ddc808eaf4e90bbb9b7cb33053f04ef9638
Yaseen Halabi <yaseenhalabi@gmail.com>
2024-08-12 21:34:35 -0500

    feat: added downloadAudio and playAudio features to textToSpeech page

--- src/screens/TextToSpeech.jsx
+++ src/screens/TextToSpeech.jsx
@@ -3,7 +3,9 @@ import '../styles/screens/TextToSpeech.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 export default function TextToSpeech() {
+    
     const user = useSelector(state => state.user.user)
+    const [audioFile, setAudioFile] = useState(null)
     useEffect(() => {
         console.log(user)
     }, [user])
@@ -16,26 +18,41 @@ export default function TextToSpeech() {
             console.error(error)
         });
     }
-    const submitText = () => {
+    const getAudio = () => {
+        console.log("getting audio")
         axios.post('http://localhost:3000/text-to-speech', { text }, {
             withCredentials: true,
             responseType: 'blob',  // Expect a blob response (binary data)
         })
         .then(response => {
             // Create a URL for the blob data
-            const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'output.mp3'); // Specify the filename
-            document.body.appendChild(link);
-            link.click();
-            link.remove(); // Remove the link from the document
+            setAudioFile(response.data)
         })
         .catch(error => {
             console.error(error);
         });
     };
-    
+
+    const downloadAudio = () => {
+        if (!audioFile) {
+            return;
+        }
+        const url = window.URL.createObjectURL(new Blob([audioFile]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'output.mp3'); // Specify the filename
+        document.body.appendChild(link);
+        link.click();
+        link.remove(); // Remove the link from the document
+    }
+    const playAudio = () => {
+        if (!audioFile) {
+            return;
+        }
+        const audio = new Audio(URL.createObjectURL(new Blob([audioFile])));
+        audio.play();
+    }
+
     const [text, setText] = useState('')
     return (
         <div className="page-container">
@@ -55,7 +72,9 @@ export default function TextToSpeech() {
                     cols='50'
                     placeholder='Type anything and turn it into natural-sounding speech' 
                 />
-                <button onClick={submitText}>Submit</button>
+                <button onClick={getAudio}>Generate Speech</button>
+                <button onClick={playAudio}>Play Speech</button>
+                <button onClick={downloadAudio}>Download Speech</button>
             </div>
         </div>
     )
